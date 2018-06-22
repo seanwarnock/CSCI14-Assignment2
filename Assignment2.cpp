3d9f7161d1ee7251042ffbf4bea90bcfb2bf69c5
Sean <swarnock@warnocksolutions.com>
2018-06-22 08:09:25 -0700

    First commit
    Base program layout

---
+++ Assignment2.cpp
@@ -0,0 +1,111 @@
+/*
+Sean Warnock
+CSCI 14 Summer 2018
+Assignment 2 due 6/28/2018
+https://github.com/seanwarnock/CSCI14-Assignment2
+*/
+
+
+/*
+Start program
+output order menu
+tell user amount to get next discount.
+take input from user until user selects "done"
+
+output invoice.
+ask user if you want another order?
+*/
+#include <iostream>
+#include <iomanip>
+#ifdef _WIN32
+  #include <windows.h>
+  using namespace std;
+#endif
+
+using namespace std;
+
+int main ()
+{
+
+//Local variables
+  const float floatMilkChocolate = 8.50;
+  const float floatDarkEuropeanChocolate = 9.75;
+  const float floatWhiteChocolate = 10.50;
+  const float floatEuopeanTruffles = 12.50;
+  float floatDiscountRate = 0;
+
+  const float floatShippRate = .10;
+
+  int intMenuChoice;
+
+  char charRunAgain;
+
+  do
+  {
+
+    char boolCompleteOrder = false;
+    do
+    {
+      system("cls");
+      //Description  - Price - Ext Price
+      cout << "[1] Milk Chocolate $" << "" << floatMilkChocolate << " per pound\n";
+      cout << "[2] Dark European Chocolate $" << "" << floatDarkEuropeanChocolate << " per pound\n";
+      cout << "[3] WhiteChocolate $" << "" << floatWhiteChocolate << " per pound\n";
+      cout << "[4] Euopean Truffles $" << "" << floatEuopeanTruffles << " per pound\n";
+      cout << "[9] Complete Order?";
+
+      cin >> intMenuChoice;
+
+      switch (intMenuChoice)
+      {
+        case 1 : cout << "Milk";
+          break;
+        case 2 : cout << "Dark";
+          break;
+        case 3 : cout << "White";
+          break;
+        case 4 : cout << "Truffles";
+          break;
+        case 9 : boolCompleteOrder = true;
+
+      }
+
+    }
+    while (boolCompleteOrder = false);
+    /* Discount rates
+  0 - 19.99 %0
+  20 - 39.99 %10
+  40 - 59.99 %15
+  60 - 79.99 %20
+  80 > %25
+  */
+
+  //for forcing the range to a integer use multiply by 100 to make my dollar amount an integer.
+
+    system("cls");
+    cout.precision(2);
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.setf(ios::showpoint);
+
+
+
+//consider trying to use justification formatting for ios
+    cout << "Invoice Order\n";
+
+    cout << "lbs of Milk Chocolate Ordered @ $" << floatMilkChocolate << " per LB\n";
+    cout << "lbs of Dark European Chocolate Ordered @ $" << floatDarkEuropeanChocolate << " per LB\n";
+    cout << "lbs of White Chocolate Ordered @ $" << floatWhiteChocolate << " per LB\n";
+    cout << "lbs of Euopean Truffles Ordered @ $" << floatEuopeanTruffles << " per LB\n";
+
+    cout << "Total Order is" << "$" << "Will be sum of order pre discount\n";
+    cout << "Less: Discount of " << floatDiscountRate << "%\n";
+
+    cout << "Order is" << "Some order total\n";
+    cout << "Shipping and Handling @ " << (floatShippRate * 10) << "%\n";
+
+    cout << "Should I really ask if you want more Chocolate? [Y] or N: ";
+    cin >> charRunAgain;
+  }
+  while (charRunAgain == 'Y');
+  return 0; //Yup still here so stdout doesn't show this program exiting with an error.
+}
